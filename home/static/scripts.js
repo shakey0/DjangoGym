7c56a0a04f7de161a5a66933376e6ff1f6341a6a
Andrew <shakeyshakespeare0@gmail.com>
2024-03-09 17:07:12 +0000

    Various buttons added to pages

--- home/static/scripts.js
+++ home/static/scripts.js
@@ -2,26 +2,58 @@ function adjustClassesForScreenWidth() {
 
     if (window.innerWidth <= 430) {
 
-        var classesTitleDiv = document.getElementById('classes-title-div');
-        if (classesTitleDiv) {
-            classesTitleDiv.classList.remove('align-items-center');
+        var titleDiv = document.getElementById('title-div');
+        if (titleDiv) {
+            titleDiv.classList.remove('align-items-center');
         }
-        var classesAdminMainButtons = document.getElementById('classes-admin-main-buttons');
-        if (classesAdminMainButtons) {
-            classesAdminMainButtons.classList.remove('ms-auto');
-            classesAdminMainButtons.classList.add('adjust-admin-main-buttons-vertical', 'mb-5');
+        var adminMainButtons = document.getElementById('admin-main-buttons');
+        if (adminMainButtons) {
+            adminMainButtons.classList.remove('ms-auto');
+            adminMainButtons.classList.add('adjust-admin-main-buttons-vertical', 'mb-5');
+        }
+        var adminMainButtonsProfile = document.getElementById('admin-main-buttons-profile');
+        if (adminMainButtonsProfile) {
+            adminMainButtonsProfile.classList.remove('ms-auto');
         }
 
     } else {
 
-        var classesTitleDiv = document.getElementById('classes-title-div');
-        if (classesTitleDiv) {
-            classesTitleDiv.classList.add('align-items-center');
+        var titleDiv = document.getElementById('title-div');
+        if (titleDiv) {
+            titleDiv.classList.add('align-items-center');
         }
-        var classesAdminMainButtons = document.getElementById('classes-admin-main-buttons');
-        if (classesAdminMainButtons) {
-            classesAdminMainButtons.classList.add('ms-auto');
-            classesAdminMainButtons.classList.remove('adjust-admin-main-buttons-vertical', 'mb-5');
+        var adminMainButtons = document.getElementById('admin-main-buttons');
+        if (adminMainButtons) {
+            adminMainButtons.classList.add('ms-auto');
+            adminMainButtons.classList.remove('adjust-admin-main-buttons-vertical', 'mb-5');
+        }
+        var adminMainButtonsProfile = document.getElementById('admin-main-buttons-profile');
+        if (adminMainButtonsProfile) {
+            adminMainButtonsProfile.classList.add('ms-auto');
+        }
+    }
+
+    if (window.innerWidth <= 630) {
+
+        var titleDivHome = document.getElementById('title-div-home');
+        if (titleDivHome) {
+            titleDivHome.classList.remove('align-items-center');
+        }
+        var adminMainButtonsHome = document.getElementById('admin-main-buttons-home');
+        if (adminMainButtonsHome) {
+            adminMainButtonsHome.classList.remove('ms-auto');
+            adminMainButtonsHome.classList.add('adjust-admin-main-buttons-vertical-630w', 'mb-5');
+        }
+    } else {
+
+        var titleDivHome = document.getElementById('title-div-home');
+        if (titleDivHome) {
+            titleDivHome.classList.add('align-items-center');
+        }
+        var adminMainButtonsHome = document.getElementById('admin-main-buttons-home');
+        if (adminMainButtonsHome) {
+            adminMainButtonsHome.classList.add('ms-auto');
+            adminMainButtonsHome.classList.remove('adjust-admin-main-buttons-vertical-630w', 'mb-5');
         }
     }
 }

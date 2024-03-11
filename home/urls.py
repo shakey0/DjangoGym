d741a4062b9abb653c9fb9c4476bda2372c69788
Andrew <shakeyshakespeare0@gmail.com>
2024-03-11 23:02:54 +0000

    Reorder route for homepage images done

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='index'),
     path('edit_selection/', views.EditSelection.as_view(), name='edit_selection'),
     path('edit_image/<int:pk>/', views.EditImage.as_view(), name='edit_image'),
+    path('sort_images/', views.sort_images, name='sort_images'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import render, redirect
 from .models import Image
 from django.views.generic import ListView, UpdateView
 from .forms import EditImageForm
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db import transaction
 
 
 class Home(ListView):
@@ -9,14 +13,42 @@ class Home(ListView):
     context_object_name = 'images'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class EditSelection(ListView):
     model = Image
     template_name = 'edit_selection.html'
     context_object_name = 'images'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class EditImage(UpdateView):
     model = Image
     form_class = EditImageForm
     template_name = 'edit_image.html'
     success_url = '/edit_selection/'
+
+
+@staff_member_required
+def sort_images(request):
+    if request.method == 'POST':
+        form_data = request.POST
+        image_ids = [item.split('_')[1] for item in form_data if item.startswith('image')]
+        images_dict = {str(image_obj.pk): image_obj for image_obj in Image.objects.filter(pk__in=image_ids)}
+        images_to_update = []
+        for item in form_data:
+            if item.startswith('image'):
+                image_id = item.split('_')[1]
+                new_order = str(int(form_data[item]) * 100)
+                if image_id in images_dict:
+                    image_instance = images_dict[image_id]
+                    image_instance.order = new_order
+                    images_to_update.append(image_instance)
+        if images_to_update:
+            with transaction.atomic():
+                Image.objects.bulk_update(images_to_update, ['order'])
+        return redirect('home:index')
+    images = Image.objects.all()
+    context = {
+        'images': images,
+    }
+    return render(request, 'sort_images.html', context)
